02a4915994e6f5b5972b0deb34c09200f5653eb6
maryamsalehi <maryamsalehi938@gmail.com>
2020-12-30 18:40:28 +0330

    added dotEnv

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const path = require('path');
 
 const express = require('express');
+const dotenv =  require('Dotenv');
 
 const app =  express();
 
 const indexRoutes = require('./routes');
+
+//* Load Config
+dotenv.config({path : "./config/config.env"});
 
 //* View Engine
 app.set('view engine','ejs');
@@ -16,4 +20,6 @@ app.use(express.static(path.join(__dirname,"public")));
 //*Routes
 app.use(indexRoutes);
 
-app.listen(3000,() => console.log("server runinng"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,() => console.log(`server runinng on ${process.env.NODE_ENV} port ${PORT}`));
