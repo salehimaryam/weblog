c344206a1d59aa3aea3c436d4b7b95f9a0fcfd79
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-01 17:34:19 +0330

    main routes

---
+++ routes/blog.js
@@ -0,0 +1,14 @@
+const {Router} = require('express');
+
+const router = new Router();
+
+//@desc Weblog Index Page
+//@route GET /
+router.get("/",(req,res)=>{
+    res.render("index", {
+        pageTitle:"وبلاگ",
+        path :"/",
+    });
+});
+
+module.exports = router;
