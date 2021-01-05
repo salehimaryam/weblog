14683af371a0e06c52a71d2776d197a06f51efca
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-05 19:00:37 +0330

    added yup

--- routes/users.js
+++ routes/users.js
@@ -1,28 +1,48 @@
-const {Router} = require("express");
+const { Router } = require("express");
+const Yup = require("yup");
 
 const router = new Router();
 
-//@ desc Login page
-//@ route GET/users/login
-router.get("/login",(req,res) => {
-    res.render("login",{
-        pageTitle:"ورود به بخش مدیریت" ,
-        path :"/login"});
+const schema = Yup.object().shape({
+    fullname: Yup.string()
+        .required("نام و نام خانوادگی الزامی می باشد")
+        .min(4)
+        .max(255),
+    email: Yup.string().email("ایمیل معتبر نمی باشد").required(),
+    password: Yup.string().min(4).max(255).required(),
+    confirmPassword: Yup.string()
+        .required()
+        .oneOf([Yup.ref("password"), null]),
 });
 
-//@ desc Login page
-//@ route GET/users/register
-router.get("/register",(req,res) => {
-    res.render("register",{
-        pageTitle:"ثبت نام کاربر جدید" ,
-        path :"/register"});
+//  @desc   Login Page
+//  @route  GET /users/login
+router.get("/login", (req, res) => {
+    res.render("login", { pageTitle: "ورود به بخش مدیریت", path: "/login" });
 });
 
-//@ desc Login Handle
-//@ route POST/users/register
-router.post("/register",(req,res) => {
-    console.log(req.body);
-    res.send("Weblog");
+//  @desc   Register Page
+//  @route  GET /users/register
+router.get("/register", (req, res) => {
+    res.render("register", {
+        pageTitle: "ثبت نام کاربر جدید",
+        path: "/register",
+    });
+});
+
+//  @desc   Register Handle
+//  @route  POST /users/register
+router.post("/register", (req, res) => {
+    schema
+        .validate(req.body)
+        .then((result) => {
+            console.log(result);
+            res.send("All Good");
+        })
+        .catch((err) => {
+            console.log(err);
+            res.send("Error", { errors: err.errors });
+        });
 });
 
 module.exports = router;
