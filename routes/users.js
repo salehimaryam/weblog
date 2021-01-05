2441d06712ee86afff554bdd7b6c5a7e518464da
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-05 22:27:57 +0330

    debog

--- routes/users.js
+++ routes/users.js
@@ -6,12 +6,17 @@ const router = new Router();
 const schema = Yup.object().shape({
     fullname: Yup.string()
         .required("نام و نام خانوادگی الزامی می باشد")
-        .min(4)
-        .max(255),
-    email: Yup.string().email("ایمیل معتبر نمی باشد").required(),
-    password: Yup.string().min(4).max(255).required(),
+        .min(4, "نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد")
+        .max(255, "نام و نام خانوادگی نباید بیشتر از 255 کاراکتر باشد"),
+    email: Yup.string()
+        .email("ایمیل معتبر نمی باشد")
+        .required("ایمیل الزامی می باشد"),
+    password: Yup.string()
+        .min(4, "کلمه عبور نباید کمتر از 4 کاراکتر باشد")
+        .max(255, "کلمه عبور نباید بیشتر از 255 کاراکتر باشد")
+        .required("کلمه عبور الزامی می باشد"),
     confirmPassword: Yup.string()
-        .required()
+        .required("تکرار کلمه عبور الزامی می باشد")
         .oneOf([Yup.ref("password"), null]),
 });
 
@@ -35,13 +40,15 @@ router.get("/register", (req, res) => {
 router.post("/register", (req, res) => {
     schema
         .validate(req.body)
-        .then((result) => {
-            console.log(result);
-            res.send("All Good");
+        .then(() => {
+            res.redirect("/users/login");
         })
         .catch((err) => {
-            console.log(err);
-            res.send("Error", { errors: err.errors });
+            res.render("register", {
+                pageTitle: "ثبت نام کاربر",
+                path: "/register",
+                errors: err.errors,
+            });
         });
 });
 
