11e5a619b4c1b646c4783b42884becd6909d82bd
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-07 14:07:44 +0330

    added controller

--- routes/users.js
+++ routes/users.js
@@ -1,47 +1,18 @@
 const { Router } = require("express");
 
-const User = require("../models/User");
+const userController =  require("../controllers/userController");
 
 const router = new Router();
 
 //  @desc   Login Page
 //  @route  GET /users/login
-router.get("/login", (req, res) => {
-    res.render("login", { pageTitle: "ورود به بخش مدیریت", path: "/login" });
-});
+router.get("/login", userController.login);
 
 //  @desc   Register Page
 //  @route  GET /users/register
-router.get("/register", (req, res) => {
-    res.render("register", {
-        pageTitle: "ثبت نام کاربر جدید",
-        path: "/register",
-    });
-});
+router.get("/register", userController.register);
 
 //  @desc   Register Handle
 //  @route  POST /users/register
-router.post("/register", async (req, res) => {
-    try {
-        await User.userValidation(req.body);
-        //await User.create(req.body);
-        res.redirect("/users/login");
-    } catch (err) {
-        console.log(err);
-        const errors = [];
-        err.inner.forEach((e) => {
-            errors.push({
-                name: e.path,
-                message: e.message,
-            });
-        });
-
-        return res.render("register", {
-            pageTitle: "ثبت نام کاربر",
-            path: "/register",
-            errors,
-        });
-    }
-});
-
+router.post("/register", userController.createUser);
 module.exports = router;
