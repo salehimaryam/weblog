f212b74b4c8964657555d33e173effc8223fae13
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-07 10:06:31 +0330

    added yup

--- models/User.js
+++ models/User.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const Yup = require("yup");
 
-const UserSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({
     fullname:{
         type:String,
-        required:true,
+        required:[true,"نام و نام خانوادگی الزامی می باشد"],
         trim:true,
     },
     email:{
@@ -23,5 +24,26 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
-const User = mongoose.model("User",UserSchema);
+const schema = Yup.object().shape({
+    fullname: Yup.string()
+        .required("نام و نام خانوادگی الزامی می باشد")
+        .min(4, "نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد")
+        .max(255, "نام و نام خانوادگی نباید بیشتر از 255 کاراکتر باشد"),
+    email: Yup.string()
+        .email("ایمیل معتبر نمی باشد")
+        .required("ایمیل الزامی می باشد"),
+    password: Yup.string()
+        .min(4, "کلمه عبور نباید کمتر از 4 کاراکتر باشد")
+        .max(255, "کلمه عبور نباید بیشتر از 255 کاراکتر باشد")
+        .required("کلمه عبور الزامی می باشد"),
+    confirmPassword: Yup.string()
+        .required("تکرار کلمه عبور الزامی می باشد")
+        .oneOf([Yup.ref("password"), null]),
+});
+
+userSchema.static.userValidation = function(body){
+   return schema.validate(body,{abortEarly:false});
+} 
+
+const User = mongoose.model("User",userSchema);
 module.export = User;
