3c4fbabd2bc1403e04db4dbff0974822e01ec7b0
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-07 14:02:34 +0330

    added User Controller

---
+++ controllers/userController.js
@@ -0,0 +1,32 @@
+const User = require("../models/User");
+
+exports.login = (req,res) => {
+   res.render("login", { pageTitle: "ورود به بخش مدیریت", path: "/login" });
+};
+
+exports.register = (req,res) => {
+   res.render("register", { pageTitle: "ثبت نام کاربر جدید", path: "/register",});
+};
+
+exports.createUser = async (req,res) => {
+   try {
+      await User.userValidation(req.body);
+      //await User.create(req.body);
+      res.redirect("/users/login");
+  } catch (err) {
+      console.log(err);
+      const errors = [];
+      err.inner.forEach((e) => {
+          errors.push({
+              name: e.path,
+              message: e.message,
+          });
+      });
+
+      return res.render("register", {
+          pageTitle: "ثبت نام کاربر",
+          path: "/register",
+          errors,
+      });
+  }
+};
