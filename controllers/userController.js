3474ae5c1a63edd31dbbd7487d49498832aabec5
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-10 21:08:51 +0330

    refactoring

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,21 +3,22 @@ const fetch = require("node-fetch");
 
 const User = require("../models/User");
 
-exports.login = (req,res) => {
-   res.render("login", {
+exports.login = (req, res) => {
+    res.render("login", {
         pageTitle: "ورود به بخش مدیریت",
         path: "/login",
-        message:req.flash("success_msg"),
+        message: req.flash("success_msg"),
         error: req.flash("error"),
     });
 };
 
-exports.handleLogin = async (req,res,next) => {
+exports.handleLogin = async (req, res, next) => {
     if (!req.body["g-recaptcha-response"]) {
         req.flash("error", "اعتبار سنجی captcha الزامی می باشد");
         return res.redirect("/users/login");
     }
-    const secretKey = process.env.CAPTCH_SECRET;
+
+    const secretKey = process.env.CAPTCHA_SECRET;
     const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body["g-recaptcha-response"]}
     &remoteip=${req.connection.remoteAddress}`;
 
@@ -42,61 +43,63 @@ exports.handleLogin = async (req,res,next) => {
     }
 };
 
-exports.rememberMe = (req,res) => {
-    if(req.body.remember){
-      req.session.cookie.originalMaxAge = 24 * 60 * 60 * 1000 //1 day
-    }else{
-    req.session.cookie.expire = null;
+exports.rememberMe = (req, res) => {
+    if (req.body.remember) {
+        req.session.cookie.originalMaxAge = 24 * 60 * 60 * 1000; // 1 day 24
+    } else {
+        req.session.cookie.expire = null;
     }
+
     res.redirect("/dashboard");
 };
 
-exports.logout = (req,res) => {
+exports.logout = (req, res) => {
     req.logout();
-    req.flash("success_msg","خروج موفقیت آمیز بود.");
+    req.flash("success_msg", "خروج موفقیت آمیز بود");
     res.redirect("/users/login");
 };
 
-exports.register = (req,res) => {
-   res.render("register", { 
-       pageTitle: "ثبت نام کاربر جدید", 
-       path: "/register",});
+exports.register = (req, res) => {
+    res.render("register", {
+        pageTitle: "ثبت نام کاربر جدید",
+        path: "/register",
+    });
 };
 
-exports.createUser = async (req,res) => {
+exports.createUser = async (req, res) => {
     const errors = [];
     try {
-      await User.userValidation(req.body);
-      const {fullname,email,password} = req.body;
-      const user = await User.findOne({email});
-      if(user){
-         errors.push({message:"کاربری با این ایمیل وجود دارد"});
-      return res.render("register", {
-        pageTitle: "ثبت نام کاربر",
-        path: "/register",
-        errors,
+        await User.userValidation(req.body);
+        const { fullname, email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (user) {
+            errors.push({ message: "کاربری با این ایمیل موجود است" });
+            return res.render("register", {
+                pageTitle: "ثبت نام کاربر",
+                path: "/register",
+                errors,
+            });
+        }
+
+        // const hash = await bcrypt.hash(password, 10);
+        // await User.create({ fullname, email, password: hash });
+        await User.create({ fullname, email, password });
+        req.flash("success_msg", "ثبت نام موفقیت آمیز بود.");
+        res.redirect("/users/login");
+    } catch (err) {
+        console.log(err);
+        err.inner.forEach((e) => {
+            errors.push({
+                name: e.path,
+                message: e.message,
+            });
+        });
+
+        return res.render("register", {
+            pageTitle: "ثبت نام کاربر",
+            path: "/register",
+            errors,
         });
     }
- 
-    //const hash = await bcrypt.hash(password,10);
-    //await User.create({fullname,email,password:hash});
-    await User.create({ fullname, email, password });
-    req.flash("success_msg","پیام موفقیت آمیز بود");
-    res.redirect("/users/login");
-
-    } catch (err) {
-      console.log(err);
-      err.inner.forEach((e) => {
-          errors.push({
-              name: e.path,
-              message: e.message,
-          });
-      });
-
-      return res.render("register", {
-          pageTitle: "ثبت نام کاربر",
-          path: "/register",
-          errors,
-      });
-  }
 };
