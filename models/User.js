1312b4624bed93995d1fa785b5ff6661133b0197
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-10 15:42:01 +0330

    create collection Blog

--- models/User.js
+++ models/User.js
@@ -44,6 +44,5 @@ userSchema.pre("save", function (next) {
     });
 });
 
-const User = mongoose.model("User", userSchema);
 
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
