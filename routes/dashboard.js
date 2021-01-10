702bb0a12cf992757c5a160cb331cc9bd1a4f0b0
maryamsalehi <maryamsalehi938@gmail.com>
2021-01-10 22:40:42 +0330

    added add-post route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,12 +1,16 @@
 const {Router} = require("express");
 const {authenticated} = require("../middelwares/auth");
 
-const admicController = require("../controllers/adminController");
+const adminController = require("../controllers/adminController");
 
 const router = new Router();
 
 //@ desc Dashboard
 //@ route GET/dashboard
-router.get("/",authenticated,admicController.getDashboard);
+router.get("/",authenticated,adminController.getDashboard);
+
+//@ desc Add Post
+//@ route GET/dashboard/Add-Post
+router.get("/add-post", authenticated, adminController.getAddPost);
 
 module.exports = router;
